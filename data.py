d05fdb266d1a7b9463460047f9267bc399de863e
Joseph Yusufov <jyusufov00@stuy.edu>
2020-05-02 18:35:42 -0400

    first working data visualization

--- data.py
+++ data.py
@@ -4,35 +4,35 @@ import sys
 
 globalTerrorism = []
 with open('./data/global-terrorism.csv', newline='') as globalTerrorismCsv:
-    reader = csv.reader(globalTerrorismCsv, delimiter=',')
+    reader = csv.DictReader(globalTerrorismCsv, delimiter=',')
     for row in reader:
         globalTerrorism.append(row)
 
 
 happiness = []
 with open('./data/happiness.csv', newline='') as happinessCsv:
-    reader = csv.reader(happinessCsv, delimiter=',')
+    reader = csv.DictReader(happinessCsv, delimiter=',')
     for row in reader:
         happiness.append(row)
 
 
 humanFreedomIndex = []
 with open('./data/human-freedom-index.csv', newline='') as humanFreedomIndexCsv:
-    reader = csv.reader(humanFreedomIndexCsv, delimiter=',')
+    reader = csv.DictReader(humanFreedomIndexCsv, delimiter=',')
     for row in reader:
         humanFreedomIndex.append(row)
 
 
 unemployment = []
 with open('./data/unemployment.csv', newline='') as unemploymentCsv:
-    reader = csv.reader(unemploymentCsv, delimiter=',')
+    reader = csv.DictReader(unemploymentCsv, delimiter=',')
     for row in reader:
         unemployment.append(row)
 
 
 universityRankings = []
 with open('./data/uni-rankings.csv', newline='') as universityRankingsCsv:
-    reader = csv.reader(universityRankingsCsv, delimiter=',')
+    reader = csv.DictReader(universityRankingsCsv, delimiter=',')
     for row in reader:
         universityRankings.append(row)
 
