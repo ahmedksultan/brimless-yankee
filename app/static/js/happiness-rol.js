424b043ed23c8eb93cb6005597ae9f42c7cfe4df
vsofat <vsofat00@stuy.edu>
2020-05-07 05:30:10 -0400

    happy v rol pt2

---
+++ app/static/js/happiness-rol.js
@@ -0,0 +1,87 @@
+// setTimeout(() => console.log(data), 2500);
+var rendered = 0;
+
+const relation = function(data){
+    console.log("hi");
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 40, left: 50},
+        width = 600 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#viz")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Parse the Data
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data.map((d) => d["pf_rol"]))])
+        // .domain([0, 5])
+        .range([ 0, width])
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+    svg.append("text")
+        .attr("transform",
+              "translate(" + (width/2) + " ," +
+                             (height + 40) + ")")
+        .style("text-anchor", "middle")
+        .text(`Extent to which ROL contributes to the
+        calculation of the Happiness Score.`);
+
+    // Y axis
+    var y = d3.scaleLinear()
+    .domain([0, d3.max(data.map((d) => d["Happiness.Score"]))])
+    // .domain([0, 10])
+    .range([height, 0])
+    // .padding(1)
+    svg.append("g")
+    .call(d3.axisLeft(y));
+    svg.append("text")
+        .attr("transform",
+            "translate(" + (-margin.right) + " ," +
+                            (height/2) + ")" +
+            "rotate(-90)")
+        .style("text-anchor", "middle")
+        .text("Happiness Score");
+
+
+    // // Lines
+    // svg.selectAll("myline")
+    // .data(data)
+    // .enter()
+    // .append("line")
+    //     .attr("x1", function(d) { return x(d["Happiness.Score"]); })
+    //     .attr("x2", x(0))
+    //     .attr("y1", function(d) { return y(d.Country); })
+    //     .attr("y2", function(d) { return y(d.Country); })
+    //     .attr("stroke", "grey")
+
+    // Circles
+    svg.selectAll("mycircle")
+    .data(data)
+    .enter()
+    .append("circle")
+        .attr("cx", function(d) { return x(d["pf_rol"]); })
+        .attr("cy", function(d) { return y(d["Happiness.Score"]); })
+        .attr("r", "3")
+        .style("fill", "#69b3a2")
+        .attr("stroke", "black")
+}
+
+
+const renderROL = function(){
+    if (rendered === 0){
+        relation(data.happinessROL);
+        rendered = 1;
+    }
+}
