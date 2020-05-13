c8a9f2832160fcbf80b9562055e4c63edfd4153d
vsofat <vsofat00@stuy.edu>
2020-05-13 01:32:32 -0400

    terrorism page updates

--- app/static/js/happiness-terrorism.js
+++ app/static/js/happiness-terrorism.js
@@ -1,7 +1,7 @@
 // setTimeout(() => console.log(data), 2500);
 var rendered = 0;
 
-const countriesLollipop = function (data) {
+const countriesLollipop = function (data, var1, var2, var3, var4) {
     console.log("hi");
     // set the dimensions and margins of the graph
     var margin = { top: 10, right: 30, bottom: 40, left: 50 },
@@ -19,8 +19,8 @@ const countriesLollipop = function (data) {
 
     // Add X axis
     var x = d3.scaleLinear()
-        .domain([0, 35])
-        // .domain([0, 5])
+        .domain([0, d3.max(data.map((d) => d[var2]))])
+        // .domain([0, 35])
         .range([0, width])
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
@@ -33,11 +33,11 @@ const countriesLollipop = function (data) {
             "translate(" + (width / 2) + " ," +
             (height + 40) + ")")
         .style("text-anchor", "middle")
-        .text(`Instances of Terrorism in 2017`);
+        .text();
 
     // Y axis
     var y = d3.scaleLinear()
-        .domain([0, d3.max(data.map((d) => d["Happiness.Score"]))])
+        .domain([0, d3.max(data.map((d) => d[var1]))])
         // .domain([0, 10])
         .range([height, 0])
     // .padding(1)
@@ -49,7 +49,7 @@ const countriesLollipop = function (data) {
             (height / 2) + ")" +
             "rotate(-90)")
         .style("text-anchor", "middle")
-        .text("Happiness Score in 2017");
+        .text();
 
 
     // // Lines
@@ -69,8 +69,8 @@ const countriesLollipop = function (data) {
     .enter()
     .append("circle")
         .transition()
-        .attr("cx", function (d) { return x(d["terrorIncidents"]); })
-        .attr("cy", function (d) { return y(d["Happiness.Score"]); })
+        .attr("cx", function (d) { return x(d[var2]); })
+        .attr("cy", function (d) { return y(d[var1]); })
         .attr("r", "4")
         .style("fill", "#b17db5")
         .attr("stroke", "black")
@@ -89,19 +89,100 @@ const countriesLollipop = function (data) {
 //                 } else {
 //                     delete row;
 //                 };
-//             });        
+//             });
 //         })
-//     ); 
+//     );
 // };
 
+const redraw = function(data, var1, var2, var3, var4) {
+    console.log("hello:", var1, ",", var2);
+    // set the dimensions and margins of the graph
+    var margin = { top: 10, right: 30, bottom: 40, left: 50 },
+        width = 600 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#viz")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    // Parse the Data
+
+    // Add X axis
+    var x = d3.scaleLinear()
+        .domain([0, d3.max(data.map((d) => d[var2]))])
+        // .domain([0, 5])
+        .range([0, width])
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+    svg.append("text")
+        .attr("transform",
+            "translate(" + (width / 2) + " ," +
+            (height + 40) + ")")
+        .style("text-anchor", "middle")
+        .text( var4 );
+
+    // Y axis
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(data.map((d) => d[var1]))])
+        // .domain([0, 10])
+        .range([height, 0])
+    // .padding(1)
+    svg.append("g")
+        .call(d3.axisLeft(y));
+    svg.append("text")
+        .attr("transform",
+            "translate(" + (-margin.right) + " ," +
+            (height / 2) + ")" +
+            "rotate(-90)")
+        .style("text-anchor", "middle")
+        .text(var3);
+
+
+    // // Lines
+    // svg.selectAll("myline")
+    // .data(data)
+    // .enter()
+    // .append("line")
+    //     .attr("x1", function(d) { return x(d["Happiness.Score"]); })
+    //     .attr("x2", x(0))
+    //     .attr("y1", function(d) { return y(d.Country); })
+    //     .attr("y2", function(d) { return y(d.Country); })
+    //     .attr("stroke", "grey")
+
+    // Circles
+    svg.selectAll("mycircle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .transition()
+        .duration(500)
+        .attr("cx", function (d) { return x(d[var2]); })
+        .attr("cy", function (d) { return y(d[var1]); })
+        .attr("r", "4")
+        .style("fill", "#b17db5")
+        .attr("stroke", "black")
+}
 
 const render = function () {
-    if (rendered === 0) {
-        rendered = 1;
-        // perCapita().then(() => {
-        //     console.log(data.happinessTerrorism); 
-        //     countriesLollipop(data.happinessTerrorism)
-        // });
-        countriesLollipop(data.happinessTerrorism);
-    }
+  
+  const viz1 = String(document.getElementById("vizSelector1").value);
+  const name1 = String(document.getElementById("vizSelector1").id);
+  const viz2 = String(document.getElementById("vizSelector2").value);
+  const name2 = String(document.getElementById("vizSelector2").id);
+  if (rendered === 0){
+      countriesLollipop(data.happiness, viz1, viz2, name1, name2);
+      rendered = 1;
+  } else {
+      d3.select("svg").remove();
+      redraw(data.happiness, viz1, viz2, name1, name2);
+  }
 }
